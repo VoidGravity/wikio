3ce753c9cad1fe64ffa5aad5d1cffe6072d116d5
VoidGravity <abdellahbardichwork@gmal.com>
2024-01-16 03:19:45 +0100

    fixing the navbar

--- app/views/inc/nav.php
+++ app/views/inc/nav.php
@@ -24,25 +24,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="no-sidebar.html">Home</a>
+                    <a class="nav-link" href="no-sidebar.php">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="no-sidebar.html">No Sidebar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="single-post.html">Single Post</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
+                
+                
+                
                 <li class="nav-item">
                     <!-- input search wide -->
                     <form class="form-inline my-3 my-lg-0">
